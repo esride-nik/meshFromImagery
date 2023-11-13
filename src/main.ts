460d90ea87a11c4315af0c05a051e9295fd2afd1
Niklas Köhn <n.koehn@esri.de>
2023-11-13 12:50:46 +0100

    map + view

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,36 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import Map from "@arcgis/core/Map";
+import SceneView from "@arcgis/core/views/SceneView";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+/********************************************************************
+ * Step 1 - Add scene with basemap *
+ ********************************************************************/
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const map = new Map({
+  basemap: "topo-3d",
+  ground: "world-elevation",
+});
+
+const view = new SceneView({
+  container: document.querySelector("#app") as HTMLDivElement,
+  center: [13.405, 52.52],
+  scale: 50000,
+  map: map,
+  camera: {
+    position: {
+      spatialReference: { latestWkid: 3857, wkid: 102100 },
+      x: 1491506.456761522,
+      y: 6892221.1440706095,
+      z: 449.1962412856519,
+    },
+    heading: 331.4896155794407,
+    tilt: 63.80679107867683,
+  },
+  environment: {
+    atmosphereEnabled: false,
+    starsEnabled: false,
+  },
+});
+
+// view.when().then(() => {
+//   view.watch("camera", (c) => console.log(JSON.stringify(c)));
+// });
